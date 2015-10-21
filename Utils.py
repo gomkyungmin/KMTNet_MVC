9212919f83c6077e19414b89d977e3a938b6cc95
gomkyungmin <kmkim82@gmail.com>
2015-10-21 11:03:38 +0900

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -12,6 +12,39 @@ def Timer(duration):
     return output
 
 
+def Tag(**args):
+
+    if args['mla_selection'] == 'rf':
+        tag = '_'+str(args['mla_selection'])\
+              +'_trees'+str(args['n_estimators'])\
+              +'_Mf'+str(args['max_features'])\
+              +'_Md'+str(args['max_depth'])\
+              +'_Mln'+str(args['max_leaf_nodes'])\
+              +'_mss'+str(args['min_samples_split'])\
+              +'_msl'+str(args['min_samples_leaf'])\
+              +'_'+str(args['criterion'])\
+              +'_'+str(args['training_part'])+str(args['training_size'])\
+              +'_scaler'+str(args['scaler'])
+
+    elif args['mla_selection'] == 'svm':
+        if args['max_iter'] == -1:
+            args['max_iter'] = 'None'
+            
+        tag = '_'+str(args['mla_selection'])\
+              +'_C'+str(args['C'])\
+              +'_k'+str(args['kernel'])\
+              +'_d'+str(args['degree'])\
+              +'_g'+str(args['gamma'])\
+              +'_c'+str(args['coef0'])\
+              +'_t'+str(args['tol'])\
+              +'_Mi'+str(args['max_iter'])\
+              +'_r'+str(args['random_state'])\
+              +'_'+str(args['training_part'])+str(args['training_size'])\
+              +'_scaler'+str(args['scaler'])
+
+    return tag
+
+
 def PickleDump(tag, clf, mla):
 
     pklfiledir = 'trained_pickle'
